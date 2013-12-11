65f4518183a0096883340124ce68eefb4ed0ac2c
Ben Bullock <bkb@cpan.org>
2013-12-11 13:46:49 +0000

    Import of BKB/IP-China-20131206 from CPAN.
    gitpan-cpan-distribution: IP-China
    gitpan-cpan-version:      20131206
    gitpan-cpan-path:         BKB/IP-China-20131206.tar.gz
    gitpan-cpan-author:       BKB
    gitpan-cpan-maturity:     released

--- lib/IP/China.pm
+++ lib/IP/China.pm
@@ -4,7 +4,7 @@ require Exporter;
 @EXPORT_OK = qw/chinese_ip/;
 use warnings;
 use strict;
-our $VERSION = '20131106';
+our $VERSION = '20131206';
 use IP::Tools;
 require XSLoader;
 XSLoader::load ('IP::China', $VERSION);
